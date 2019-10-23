4b24e5d3faf4f9708cc7b2086039e4749c4e5486
Marcelo Ramos Machado <mr.machado@gmail.com>
2019-10-23 17:48:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Photo from './components/photo';
 
-function App() {
-  return (
+// import { Container } from './styles';
+
+class App extends Component {
+  render() {
+    const imageData = {
+      src: "https://images.unsplash.com/photo-1508182314998-3bd49473002f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=af394daae4bdbc4a95dc2204984b790d&auto=format&fit=crop&w=800&q=60",
+      user: {
+        name: "Luca Bravo",
+        profile_image: "https://images.unsplash.com/profile-1441298803695-accd94000cac?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32&s=7cfe3b93750cb0c93e2f7caec08b5a41",
+        link: "https://unsplash.com/@lucabravo"
+      }
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Photo {...imageData}/>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
